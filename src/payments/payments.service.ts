ee52abeca6bd65a66764074b48a45f3551d07f1b
Nelson Cadenas <nca1478@yahoo.com>
2024-08-29 00:15:08 -0400

    Crear sesión de pago

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -5,4 +5,31 @@ import { envs } from '../config';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession() {
+    const session = await this.stripe.checkout.sessions.create({
+      // colocar el id de la orden
+      payment_intent_data: {
+        metadata: {},
+      },
+
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Teclado',
+            },
+            unit_amount: 2000, // 20 dolares -> 2000 / 100 = 20.00
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success', // url del front
+      cancel_url: 'http://localhost:3003/payments/cancel', // url del front
+    });
+
+    return session;
+  }
 }
