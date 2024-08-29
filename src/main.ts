6d698df871260290fd616250fa1403102c258939
Nelson Cadenas <nca1478@yahoo.com>
2024-08-28 23:40:02 -0400

    Payments - Microservice

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(envs.port);
+
+  logger.log(`Payments Microservice running on port ${envs.port}`);
+}
+
+bootstrap();
